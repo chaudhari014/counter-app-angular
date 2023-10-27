9e635beda2b052b07751a4cfc92b0c3eb5061b1f
chaudhari014 <fatarambhai.k14@gmail.com>
2023-10-27 20:59:51 +0530

    add sound when click

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Howl } from 'howler';
 
 @Component({
   selector: 'app-counter',
@@ -7,14 +8,23 @@ import { Component } from '@angular/core';
 })
 export class CounterComponent {
   counter = 0;
+  Sound: Howl;
+
+  constructor() {
+    this.Sound = new Howl({
+      src: ['../../assets/clickSound.wav'], // Update with the actual path
+    });
+  }
   increment() {
     this.counter++;
+    this.Sound.play();
   }
   decrement() {
     if (this.counter === 0) {
       return;
     }
     this.counter--;
+    this.Sound.play();
   }
   reset() {
     this.counter = 0;
